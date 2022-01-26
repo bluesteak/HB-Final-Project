62dbd0df06266cc2e5ac6bb8b099237c116f80a9
Quyen <qtruongk@gmail.com>
2022-01-26 04:31:42 +0000

    Working on Model.py

--- model.py
+++ model.py
@@ -3,11 +3,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-db = SQAlchemy()
+db = SQLAlchemy()
 
 class Movie(db.Model):
-    """A movie."""
-
+    """"A movie."""
     __tablename__ = "movies"
 
     movie_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
@@ -22,32 +21,40 @@ class Movie(db.Model):
     def __repr__(self):
         return f'<Movie movie_id={self.movie_id} title={self.movie_title}>'
 
-class Characters(db.Model):
-    """Movie's character."""
-
+class Character(db.Model):
+    """"Movie's character."""
     __tablename__ = "characters"
 
     char_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
     char_name = db.Column(db.String)
     movie_id = db.Column(db.Integer, db.ForeignKey("movies.movie_id"))
- 
+    actor_id = db.Column(db.Integer, db.ForeignKey("actors.actor_id"))
 
 
     movie = db.relationship("Movie", backref="characters")
+    actor = db.relationship("Actor",backref="characters")
 
     def __repr__(self):
-        return f'<Characters char_id={self.char_id} >'
+        return f'<Character char_id={self.char_id} char_name={self.char_name}>'
+ 
     
+class Actor(db.Model):
 
-def connect_to_db(flask_app, db_uri="postgresql:///movies", echo=True):
+    __tablename__ = "actors"
+
+    actor_id = db.Column(db.Integer, autoincrement=True, primary_key=True)
+    actor_name = db.Column(db.String)
+
+    def __repr__(self):
+        return f'<Actor actor_id={self.actor_id} actor_name={self.actor_name}>'
+
+def connect_to_db(flask_app, db_uri="postgresql:///actors", echo=True):
     flask_app.config["SQLALCHEMY_DATABASE_URI"] = db_uri
     flask_app.config["SQLALCHEMY_ECHO"] = echo
     flask_app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.app = flask_app
     db.init_app(flask_app)
-
-    print("Connected to the db!")
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -5,7 +5,8 @@ from flask import Flask
 from flask import (Flask, render_template, request, flash, session,
                    redirect)
 
-from jinja2 import StrictUndefined
+from jinja2 import StrictUndefined 
+import requests
 app = Flask(__name__)
 app.secret_key = "BakedPineapple"
 app.jinja_env.undefined = StrictUndefined
@@ -15,6 +16,20 @@ app.jinja_env.undefined = StrictUndefined
 def homepage():
     """ Show homepage """
     return render_template("homepage.html")
+
+@app.route("/movies")
+def movies():
+    
+    response = requests.get("")
+    data = response.json()
+
+@app.route("/actors")
+def actors():
+ 
+    return render_template("actors.html")
+    # response = requests.get("https://api.themoviedb.org/3/search/person?api_key=ca04f28350cde67bc24470dfe961b3dd&language=en-US&query=nguyen&page=1&include_adult=false")
+    # data = response.json()
+    # return data
 
 
 
